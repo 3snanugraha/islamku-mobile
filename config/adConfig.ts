cc3d4f4fc20054975cd18641f44cb510b7ba4f6c
Tris <trisnanugraha87@gmail.com>
2024-12-02 00:31:06 +0700

    "Update GIST_URL by removing file path from the end"

--- config/adConfig.ts
+++ config/adConfig.ts
@@ -1,4 +1,4 @@
-const GIST_URL = 'https://gist.githubusercontent.com/3snanugraha/8f7bcce3f27dd9ee99e243769dc49870/raw/e6c6d192788d9bac5b5eb3b1669893efcf9c1fa4/IslamkuAdConfig.json';
+const GIST_URL = 'https://gist.githubusercontent.com/3snanugraha/8f7bcce3f27dd9ee99e243769dc49870/raw';
 
 export interface AdConfig {
   version: string;
